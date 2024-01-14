323f20f92ffc917f99d928f269266defe5fa63dd
Sami-115667 <shamsurrahman07052001@gmail.com>
2024-01-14 20:11:09 +0600

    new code added

--- demo.py
+++ demo.py
@@ -1,64 +1,68 @@
-# print("this is my first python program")
-
-# cgpa=23
-# print (cgpa)
-# sum2= float(input(""))
-# sum=4
-# name="m name is md shamsur rahman sami "
-# print (name   +  " ",cgpa/sum2)
-
-# mark=20
-# if (mark<33):
-#     print("failed")
-#     print("sa")
-# else:
-#     print("pass")
-
-# print(51 or 7)
-# i=1
-# sum=0
-# while i<10:
-#     sum=sum+i
-#     i=i+1
-
-# print(sum)
-# num=list(range(1,100,2))
-# print(num)
-
-# num=[2,4,5,66,777,4]
-# count=0
-# while  count<len(num):
-#     print(num[count])
-#     count=count+1
-
-
-# for  x in num:
-#     print(x  ,"\n")
-
-# n= int( input())
-
-# sum=0
-
-# for x in range(1,n+1):
-#     sum=sum+(x**2)
-# print (sum)
-
-
-# n=3
-# for i in range(1,n+1,1):
-#     print(((2*i)-1)*"*")
+from random import randint 
 
 
 
-# from random import randint
+print("this is my first python program")
+
+cgpa=23
+print (cgpa)
+sum2= float(input("Enter a floating number"))
+sum1=4
+name="m name is md shamsur rahman sami "
+print (name   +  " ",cgpa/sum2)
+
+mark=20
+if (mark<33):
+    print("failed")
+    print("sa")
+else:
+    print("pass")
+
+print(51 or 7)
+i=1
+sum=0
+while i<10:
+    sum=sum+i
+    i=i+1
+
+print(sum)
+num=list(range(1,100,2))
+print(num)
+
+num=[2,4,5,66,777,4]
+count=0
+while  count<len(num):
+    print(num[count])
+    count=count+1
 
 
-# random= randint(1,10)
-# print(random)
+for  x in num:
+    print(x  ,"\n")
+
+n= int( input("Enter a Integer number "))
+
+sum=0
+
+for x in range(1,n+1):
+    sum=sum+(x**2)
+print (sum)
+
+
+n=3
+for i in range(1,n+1,1):
+    print(((2*i)-1)*"*")
+
+
+
+
+
+
+random= randint(1,10)
+print(random)
 
 print("""
-    
-                        
+
+
                   
              ______      
            _/  ( ( \_
