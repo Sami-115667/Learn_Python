793114c23b6c2caa17c7be0c6b4b8c1db4a7a6d1
Sami-115667 <shamsurrahman07052001@gmail.com>
2024-01-14 20:23:23 +0600

    new code added

---
+++ Strings.py
@@ -0,0 +1,41 @@
+name="sami"
+print(name)
+
+print("hello", name, sep="~")
+
+apple="he said, \"my name is apple\""  
+apple1='he said, "my name is apple"'
+                                             #printing double quotation in a string using \"   
+print(apple)
+print(apple1)
+
+a=len(apple)  #finding the length of string
+print(a)
+
+
+for char in name:
+    print(char)
+
+
+print(name[0:4])
+print(name[-4:])
+### Methods in string
+
+a="my name is sami"
+a=a.upper()
+b=a.lower()
+print(a)
+print(b)
+
+ab="samiieeeeeeeee"
+print(ab.rstrip("e"))
+print(ab.replace("e","!"))
+print(a.split())
+print(a.capitalize())
+print(a.count("SAMI"))
+print(a.isspace())
+
+
+
+for k in range(0,5):
+ print(k ,end=",")
